3ef12b8de97e23559f3add58a15bfe3fef1db9ae
Florin Epuras <florin.epuras@gmail.com>
2023-08-08 11:25:20 +0300

    Adjusted comments

--- Chapter 1/e1_1.c
+++ Chapter 1/e1_1.c
@@ -3,6 +3,11 @@
 //compatible with the new standard. It returns an integer.
 //Compiled in terminal using gcc -o e1_1 e1_1.c
 //Result is terminal outputs string "hello world"
+//TAKES: main() is the entry point in a C program and it is a function.
+//It is not changeable in any way and it must exists.
+//Functions communicate data between them using arguments. Not here though.
+//Escape sequence like \n - new line character, line advance
+//printf() does not supplies a newline automagically
 
 #include <stdio.h>
 int main()
